b26af514bf070527ea6ca27c80ad74d5eeecca0a
Michael Solomon <micheal540@gmail.com>
2018-07-14 12:33:17 +0300

    Testing Variation
    Enable to change buttons config according to A/B testing config

--- src/server.js
+++ src/server.js
@@ -1,11 +1,30 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const lodash = require("lodash");
 
 app.use(express.static('static'));
 
 app.get('/buttons', (req, res) => {
-  res.json(require("../config/buttonsOrder.config"));
+  const buttonsConfig = require("../config/buttons.config");
+  const experiments = require("../config/abTest.config.json").experiments;
+
+  experiments.forEach(experiment => {
+    experiment.variations.forEach(variation => {
+      const configIndex = lodash.findIndex(buttonsConfig, ["name", variation.name]);      
+
+      if(variation.color) {
+        buttonsConfig[configIndex].color = variation.color;
+      }
+
+      if(variation.switchWith) {
+        const switchWithIndex = lodash.findIndex(buttonsConfig, ["name", variation.switchWith]);      
+        [buttonsConfig[configIndex], buttonsConfig[switchWithIndex]] = [buttonsConfig[switchWithIndex], buttonsConfig[configIndex]]
+      }
+    });
+  });
+
+  res.json(buttonsConfig);
 });
 
 // main route
